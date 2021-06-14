e6365f9cf9d69d4d8a2774059224ef045ebdfe6b
MaygoDev <52964957+MaygoDev@users.noreply.github.com>
2021-06-14 18:44:20 +0200

    update for lite version

--- src/fr/maygo/howmanylines/HowManyLines.java
+++ src/fr/maygo/howmanylines/HowManyLines.java
@@ -3,7 +3,14 @@ package fr.maygo.howmanylines;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HowManyLines {
 	
@@ -14,15 +21,19 @@ public class HowManyLines {
 		
 		if (!src.exists()) {
 			System.err.println("The source folder wasn't found");
-			Thread.sleep(5000L);
-			System.exit(0);
+			return;
 		}else {
 			System.out.println("The source folder was found");
 		}
 
         search(src);
-		System.out.println("There is a Total of " + lines + " lines and "+packages+". For " + classes + " Classes.");
-		Thread.sleep(10000L);
+		System.out.println("There is a Total of " + lines + " lines, "+packages+" packages and " + classes + " classes.");
+		File log = new File(new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss").format(new Date())+".hml");
+		log.createNewFile();
+		FileWriter writer = new FileWriter(log);
+		writer.write("There is a Total of " + lines + " lines, "+packages+" packages and " + classes + " classes.");
+		writer.flush();
+		writer.close();
 	}
 
 	public static void search(final File folder) throws IOException, InterruptedException {
@@ -34,22 +45,12 @@ public class HowManyLines {
             }
 
             if (f.isFile()) {
-            	int fileLines = 0;
-            	FileReader reader = new FileReader(f);
-            	BufferedReader buffer = new BufferedReader(reader);
-            	
-            	while(buffer.readLine() != null) {
-            		fileLines++;
-            	}
-            	
-            	buffer.close();
-            	reader.close();
+            	int fileLines = Files.readAllLines(Paths.get(f.getPath())).size();
             	
             	classes++;
             	lines += fileLines;
             	
             	System.out.println("In the File "+f.getName()+", There is "+fileLines+" lines.");
-            	Thread.sleep(fileLines*2);
             }
 
         }
